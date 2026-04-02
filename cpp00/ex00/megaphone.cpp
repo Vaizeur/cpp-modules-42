67d9518bd74c607e3786ad51327b9e1bf05e995f
Vaizeur <danyliukoleksandr77270@gmail.com>
2026-04-02 09:26:48 +0200

    fix cpp00-ex00

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: odanyliu <odanyliu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/24 10:04:27 by odanyliu          #+#    #+#             */
-/*   Updated: 2026/02/24 10:04:32 by odanyliu         ###   ########.fr       */
+/*   Updated: 2026/04/02 09:26:01 by odanyliu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,17 @@ int main(int argc, char const *argv[])
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 		return (0);
 	}
-	std::string message = argv[1];
-	std::string MESSAGE = argv[1];
-	for (std::size_t i = 0; i < message.length(); i++)
-        MESSAGE[i] = toupper(message[i]);
-	std::cout << MESSAGE << std::endl;
+	std::string message;
+	std::string MESSAGE;
+	for (int i = 1; i < argc; i++)
+	{
+		message = argv[i];
+		MESSAGE = argv[i];
+		for (std::size_t y = 0; y < message.length(); y++)
+			MESSAGE[y] = toupper(message[y]);
+		std::cout << MESSAGE;
+	}
+	std::cout << std::endl;
 	return 0;
 }
 
