2972b911ef71f094aac6c181795739ea3a227c86
Vaizeur <danyliukoleksandr77270@gmail.com>
2026-04-20 10:05:51 +0200

    CPP02-Ex03 fix

--- cpp02/ex03/bsp.cpp
+++ cpp02/ex03/bsp.cpp
@@ -6,13 +6,13 @@
 /*   By: odanyliu <odanyliu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/30 11:31:46 by odanyliu          #+#    #+#             */
-/*   Updated: 2026/04/17 15:12:05 by odanyliu         ###   ########.fr       */
+/*   Updated: 2026/04/20 10:05:20 by odanyliu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Point.hpp"
 
-static float calcul_sign(Point const p1, Point const p2, Point const p3)
+static float orientation(Point const p1, Point const p2, Point const p3)
 {
     return (p1.getX().toFloat() - p3.getX().toFloat()) *
 		(p2.getY().toFloat() - p3.getY().toFloat()) -
@@ -21,8 +21,8 @@ static float calcul_sign(Point const p1, Point const p2, Point const p3)
 }
 bool bsp(Point const a, Point const b, Point const c, Point const point)
 {
-    bool b1 = calcul_sign(point, a, b) < 0;
-    bool b2 = calcul_sign(point, b, c) < 0;
-    bool b3 = calcul_sign(point, c, a) < 0;
-    return (b1 && b2 && b3);
+    bool b1 = orientation(point, a, b) < 0;
+    bool b2 = orientation(point, b, c) < 0;
+    bool b3 = orientation(point, c, a) < 0;
+    return ((b1 == b2) && (b2 == b3));
 }
