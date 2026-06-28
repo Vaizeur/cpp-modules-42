8151673361db63a4b728c5991c5e943935f54416
V4IZ <>
2026-06-28 16:58:22 +0200

    CPP08-EX01

--- cpp08/ex01/Span.cpp
+++ cpp08/ex01/Span.cpp
@@ -1,40 +1,81 @@
 
 #include "Span.hpp"
-
 #include <algorithm>
-#include <limits>
 
-Span::Span() : _maxSize(0), _min(0), _max(0) {}
+Span::Span() : _maxSize(0) , _isSorted(false) {}
 
-Span::Span(unsigned int size) : _maxSize(size), _min(0), _max(0) {}
+Span::Span(unsigned int size) : _maxSize(size) , _isSorted(false) {}
 
 Span& Span::operator=(const Span& other) {
 	if (this != &other){
 		_numbers = other._numbers;
 		_maxSize = other._maxSize;
+		_isSorted = other._isSorted;
 	}
 	return *this;
 }
 
 Span::~Span() {}
 
-void Span::addNumber(int number){
-	if (_maxSize >= _numbers.size() + 1)
-		return ;
+void Span::sort()
+{
+	if (_isSorted)
+		return;
+	std::sort(_numbers.begin(), _numbers.end());
+	_isSorted = true;
+}
+void Span::checkSpan() const
+{
+	if (_numbers.empty())
+		throw SpanEmptyException();
+	if (_numbers.size() == 1)
+		throw SpanOneElementException();
+}
+
+void Span::addNumber(const int number){
+	if (_maxSize <= _numbers.size())
+		throw SpanFullException();
+	if (_isSorted && number < _numbers.back())
+		_isSorted = false;
 	_numbers.push_back(number);
+}
+void Span::addNumbers(std::vector<int>::iterator begin, std::vector<int>::iterator end)
+{
+	if (std::distance(begin, end) + _numbers.size() > _maxSize)
+		throw SpanFullException();
+	_numbers.insert(_numbers.end(), begin, end);
+	_isSorted = false;
 }
 
 int Span::longestSpan()
 {
-	int longestSpan = 0;
-
-	std::sort(_numbers.begin(), _numbers.end());
-	std::vector<int>::iterator it;
-	for (it = _numbers.begin(); it != _numbers.end(); ++it)
-	{
-
-	}
-	return longestSpan;
+	checkSpan();
+	sort();
+	return _numbers.back() - *_numbers.begin();
 }
 
+int Span::shortestSpan()
+{
+	checkSpan();
+	sort();
+	const std::vector<int>::iterator begin = _numbers.begin();
+	const std::vector<int>::iterator end = _numbers.end();
+	int	min = *(begin + 1) - *begin;
+	for (std::vector<int>::iterator it = begin + 1; it != end; ++it) {
+		if (*it - *(it-1) < min)
+			min = *it - *(it-1);
+	}
+	return min;
+}
 
+const char* Span::SpanFullException::what() const throw() {
+	return "Full Span";
+}
+
+const char* Span::SpanEmptyException::what() const throw() {
+	return "Empty Span";
+}
+
+const char* Span::SpanOneElementException::what() const throw() {
+	return "Span contain only one Element";
+}
--- cpp08/ex01/Span.hpp
+++ cpp08/ex01/Span.hpp
@@ -1,6 +1,7 @@
 #ifndef SPAN_HPP
 # define SPAN_HPP
-#include <vector>
+# include <vector>
+# include <iostream>
 
 class Span
 {
@@ -8,16 +9,28 @@ class Span
 		Span();
 		std::vector<int> _numbers;
 		unsigned	int _maxSize;
-		int				_min;
-		int				_max;
+		bool			_isSorted;
 	public :
 		Span(unsigned int size);
 		Span(const Span &other);
 		Span& operator=(const Span &other);
 		~Span();
+		void	sort();
+		void	checkSpan() const;
 		void	addNumber(int number);
+		void	addNumbers(std::vector<int>::iterator begin, std::vector<int>::iterator end);
 		int		shortestSpan();
 		int		longestSpan();
+
+		class SpanFullException : public std::exception {
+			const char* what() const throw();
+		};
+		class SpanEmptyException : public std::exception {
+			const char* what() const throw();
+		};
+		class SpanOneElementException : public std::exception {
+			const char* what() const throw();
+		};
 };
 
 
