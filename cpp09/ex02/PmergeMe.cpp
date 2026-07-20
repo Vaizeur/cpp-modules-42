715865abe772fd475de9db17c687d16b1a3030eb
V4IZ <>
2026-07-20 10:10:55 +0200

    CPP09-EX02 Suite Jacobsthal

--- cpp09/ex02/PmergeMe.cpp
+++ cpp09/ex02/PmergeMe.cpp
@@ -44,6 +44,40 @@ void	PmergeMe::sortPairs(T &pairs) {
 	}
 }
 
+template <typename T , typename U>
+
+T	PmergeMe::generateJacobsthal(int size, U &pairs)
+{
+	(void) pairs;
+
+	typename ContainerConvert<U>::type jacob;
+	typename ContainerConvert<U>::type order;
+
+	jacob.push_back(1);
+	jacob.push_back(3);
+	int nextValue = jacob[jacob.size()-1] + 2 * jacob[jacob.size()-2];
+	while (jacob.back() < size) {
+		jacob.push_back(nextValue);
+		nextValue = jacob[jacob.size()-1] + 2 * jacob[jacob.size()-2];
+	}
+	int lastProcessed = 0;
+
+	for (size_t i = 0; i < jacob.size(); i++) {
+		int targetJacob = jacob[i] - 1;
+		if (targetJacob >= size)
+			targetJacob = size - 1;
+		for (int j = targetJacob; j > lastProcessed; --j) {
+			order.push_back(j);
+		}
+		lastProcessed = targetJacob;
+		if (lastProcessed >= size - 1)
+			break;
+	}
+	for (int i = lastProcessed + 1; i < size; i++)
+		order.push_back(i);
+	return order;
+}
+
 template< typename T>
 void	PmergeMe::sortContainer(T &container) {
 	if (container.size() <= 1)
@@ -55,6 +89,7 @@ void	PmergeMe::sortContainer(T &container) {
 	int oddValue = 0;
 	if (oddContainer)
 		oddValue = container.back();
+	(void) oddValue;
 	typename PairStorage<T>::type pairs;
 	for (size_t i = 0 ; i < container.size() - 1; i += 2){
 		const int left = container.at(i);
@@ -78,6 +113,15 @@ void	PmergeMe::sortContainer(T &container) {
 		sortedContainer.push_back(pairs[i].second);
 		waitingContainer.push_back(pairs[i].first);
 	}
+
+	T jacobsthal = generateJacobsthal<T >(waitingContainer.size(), pairs);
+
+	/*
+	std::cout << "LA SUITE " << std::endl;
+	for (typename T::iterator it = jacobsthal.begin(); it != jacobsthal.end(); ++it)
+		std::cout << *it << std::endl;
+	std::cout << "FIN DE LA SUITE " << std::endl;
+	*/
 
 	//TODO: faire la suite de Jacobsthal
 
--- cpp09/ex02/PmergeMe.hpp
+++ cpp09/ex02/PmergeMe.hpp
@@ -20,16 +20,35 @@ struct PairStorage<std::deque<int> > {
 	typedef std::deque<std::pair<int,int> > type;
 };
 
+template<typename T>
+struct ContainerConvert;
+
+template<>
+struct ContainerConvert<std::vector<std::pair<int,int > > > {
+	typedef std::vector<int> type;
+};
+
+template<>
+struct ContainerConvert<std::deque<std::pair<int,int > > > {
+	typedef std::deque<int> type;
+};
+
 class PmergeMe
 {
 	private:
 		std::vector<int>	_vec;
 		std::deque<int>		_deq;
 		PmergeMe();
+
 		template< typename T>
 		void		sortContainer(T &container);
+
 		template< typename T>
 		void		sortPairs(T &pairs);
+
+		template< typename T, typename U>
+		T			generateJacobsthal(int size, U &pairs);
+
 	public:
 		PmergeMe(int argc, char **argv);
 		PmergeMe(const PmergeMe &other);
